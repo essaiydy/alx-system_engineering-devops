489fdc63de78ea016131e24cb5fba50ac30bd9ab
essaiydy <imaneessaiydy2@gmail.com>
2023-12-22 14:00:11 -0800

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-# Using Puppet, install flask from pip3 
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+# install puppet-lint
+package{'puppet-lint':
+ensure   => '2.5.0',
+provider => 'gem'
 }
