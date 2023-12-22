226e24c86a687e46644d63d12be33d186cca2b5c
essaiydy <imaneessaiydy2@gmail.com>
2023-12-22 13:47:48 -0800

    task 1

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+#Using Puppet, install flask from pip3.
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
+}
