7172772e29f1daaf5e1a982adc2a6f24644d4308
essaiydy <imaneessaiydy2@gmail.com>
2024-01-30 08:19:00 -0800

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,29 +8,20 @@ if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/todos/' + sys.argv[1]
     usinfo = 'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
 
-    try:
-	todo_respo = requests.get(url).json()
-	name_respo = requests.get(usinfo).json()
+    todo_respo = requests.get(url).json()
+    name_respo = requests.get(usinfo).json()
 
-	name = name_respo.get("name")
-	
-	nub_todo = len(todo_respo)
+    name = name_respo.get("name")
 
-	j = 0
+    nub_todo = len(todo_respo)
 
-	for i in todo_respo:
-	    if i["userId"] == sys.argv[1] && i["completed"]:
-		j++
+    j = 0
 
-	print("Employee {} is done with tasks {}/{}".format(name, j, nub_todo))
+    for i in todo_respo:
+        if i["userId"] == sys.argv[1] && i["completed"]:
+            j++
+            print("Employee {} is done with tasks {}/{}".format(name, j, nub_todo))
 
-	for title in todo_respo:
-	    if title["completed"]:
-		print("\t {}".format(todo_respo.get("title")))
-
-    except requests.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")
-    except requests.RequestException as req_err:
-        print(f"Request error occurred: {req_err}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+    for title in todo_respo:
+        if title["completed"]:
+            print("\t {}".format(todo_respo.get("title")))
