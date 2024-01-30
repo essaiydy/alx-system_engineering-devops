0b395f360233185b75dd4141e2b5cfda336eb51e
essaiydy <imaneessaiydy2@gmail.com>
2024-01-30 09:08:35 -0800

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,14 +10,14 @@ if __name__ == "__main__":
 
     todo_respo = requests.get(url).json()
     name_respo = requests.get(usinfo).json()
-    EMPLOYEE_NAME = name_respo.get("name")
-    TOTAL_NUMBER_OF_TASKS = len(todo_respo)
-    NUMBER_OF_DONE_TASKS = 0
+    NAME = name_respo.get("name")
+    TASKS = len(todo_respo)
+    DONE = 0
 
     for task in todo_respo:
         if task["completed"]:
-            NUMBER_OF_DONE_TASKS += 1
-    print("Employee {} is done with tasks {}/{}".format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+            DONE += 1
+    print("Employee {} is done with tasks {}/{}".format(NAME, DONE, TASKS))
 
     for task in todo_respo:
         if task["completed"]:
