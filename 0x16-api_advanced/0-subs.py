3ca6b5cd63a7eb8125ed53337a6ecf46752b2bf3
essaiydy <imaneessaiydy2@gmail.com>
2024-02-13 09:11:16 -0800

    task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,11 +8,11 @@ def number_of_subscribers(subreddit):
     returns the number of subscribers
     (not active users, total subscribers) for a given subreddit'''
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': Mozilla/5.0 (Windows NT 10.0; Win64; x64)
-            AppleWebKit/537.36 (KHTML, like Gecko)
-            Chrome/58.0.3029.110 Safari/537.3}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
     response = requests.get(url, headers=headers)
     if response.status_code == 404:
         return 0
     dictt = response.json().get("data")
-    return(dictt.get("subscribers")
+    return(dictt.get("subscribers"))
