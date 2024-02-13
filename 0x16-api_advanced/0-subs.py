70ae584082bd6af8a427e22cbd22148dd7759bed
essaiydy <imaneessaiydy2@gmail.com>
2024-02-13 09:14:53 -0800

    task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,9 @@ def number_of_subscribers(subreddit):
     (not active users, total subscribers) for a given subreddit'''
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' +
+        'AppleWebKit/537.36 (KHTML, like Gecko)' +
+        'Chrome/58.0.3029.110 Safari/537.3'
     }
     response = requests.get(url, headers=headers)
     if response.status_code == 404:
