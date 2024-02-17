060a8dd22aa94c46b9204d71378ff537d960d89d
essaiydy <imaneessaiydy2@gmail.com>
2024-02-17 10:42:30 -0800

    task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-'''fonction'''
+"""queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    '''Write a function that queries the Reddit API and
-    returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit'''
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' +
-        'AppleWebKit/537.36 (KHTML, like Gecko)' +
-        'Chrome/58.0.3029.110 Safari/537.3'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' +
+            'AppleWebKit/537.36 (KHTML, like Gecko)' +
+            'Chrome/58.0.3029.110 Safari/537.3'
     }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 404:
+    r = requests.get('https://www.reddit.com/r/{:}/about.json'.format(
+        subreddit), headers=headers, allow_redirects=False)
+    if r.status_code >= 300:
         return 0
-    dictt = response.json().get("data")
-    return(dictt.get("subscribers"))
+    json = r.json()
+    data_dict = json.get('data')
+    return(data_dict.get('subscribers'))
